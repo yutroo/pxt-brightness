0e01cb4d37b38706f2e869863fb2ecb9e79953d9
yutroo <46149176+yutroo@users.noreply.github.com>
2018-12-27 14:59:08 +0900

    Change Icon

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+main.ts
+
+//% weight=70 icon=#555555 block="暗い" 
+namespace 
+   //% blockId=show_strings block="暗い %v"
+   export function noaction(text: string):void
+
+   
